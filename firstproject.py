de6b6b11e6a789234443a0a2dc45373f544119cb
twglhamo <lhamotshewang161@gmail.com>
2025-07-29 14:48:58 +0600

    my identity

--- firstproject.py
+++ firstproject.py
@@ -4,4 +4,4 @@ sum = a + b
 print("The sum of a and b is: ",sum)
 name=input("Enter your name")
 age = input("Enter your age")
-print ("you are, name," and you are " ,age,git  years old")
+print ("you are ", name," and you are " ,age," years old")
