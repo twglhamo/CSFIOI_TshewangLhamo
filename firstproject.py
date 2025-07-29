f5e2112f058df8e9e1aeac57c4e5b603283c7f00
twglhamo <lhamotshewang161@gmail.com>
2025-07-29 14:41:07 +0600

    my fourth commit

--- firstproject.py
+++ firstproject.py
@@ -1,3 +1,7 @@
-print("hello world")
-print("my name")
-print ("my age")
+a = 2
+b = 3
+sum = a + b
+print("The sum of a and b is: ",sum)
+name=input("Enter your name")
+age = input("Enter your age")
+print ("you are, name," and you are " ,age,git  years old")
