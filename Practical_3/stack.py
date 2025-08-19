7d25b3282807fa32a2095ac41a69d548abc6159a
twglhamo <lhamotshewang161@gmail.com>
2025-08-19 15:12:36 +0600

    practical 3

---
+++ Practical_3/stack.py
@@ -0,0 +1,34 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+    
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("Stack is empty")
+        
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("Stack is empty")
+        
+    def size(self):
+        return len(self.items)
+    
+    # Test the Stack
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print(stack.pop())  # Should print 3
+print(stack.peek())  # Should print 2
+print(stack.size())  # Should print 2
+    
