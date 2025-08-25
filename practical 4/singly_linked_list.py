ce5c640b7a1b6ac4f4a97a25c6ec26100e04f6d6
twglhamo <lhamotshewang161@gmail.com>
2025-08-25 14:49:41 +0600

    practical 3

---
+++ practical 4/singly_linked_list.py
@@ -0,0 +1,89 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node    
+
+    def display(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        print(" -> ".join(map(str, elements)))
+
+    def insert(self, data, position):
+        new_node = Node(data)
+        if position == 0:
+            new_node.next = self.head
+            self.head = new_node
+            return
+        current = self.head
+        for _ in range(position - 1):
+            if current is None:
+                raise IndexError("position out of range")
+            current = current.next
+        if current is None:
+            raise IndexError("position out of range")
+        new_node.next = current.next
+        current.next = new_node
+
+    def delete(self, data):
+        if not self.head:
+            return
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next:
+            if current.next.data == data:
+                current.next = current.next.next
+                return
+            current = current.next
+
+    def search(self, data):
+        current = self.head
+        position = 0
+        while current:
+            if current.data == data:
+                return position
+            current = current.next
+            position += 1
+        return -1
+
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        self.head = prev
+
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.display()       
+ll.insert(4, 1)
+ll.display()       
+ll.delete(2)
+ll.display()       
+print(ll.search(4))
+print(ll.search(5)) 
+ll.reverse()
+ll.display()        
